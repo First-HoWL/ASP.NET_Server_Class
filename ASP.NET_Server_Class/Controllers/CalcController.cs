3d5ae1f63a64bfbd570f4a0c8b2cb0205db38161
First-HoWL <154261106+First-HoWL@users.noreply.github.com>
2026-02-17 18:50:20 +0200

    Calc Controller (Class Work)

---
+++ ASP.NET_Server_Class/Controllers/CalcController.cs
@@ -0,0 +1,33 @@
+﻿using ASP.NET_Server_Class.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace ASP.NET_Server_Class.Controllers
+{
+    [ApiController]
+    [Route("calc/[controller]")]
+    public class CalcController : ControllerBase
+    {
+        [HttpPost("/sum")]
+        public ActionResult PostSum(int a, int b)
+        {
+            return Ok(a + b);
+        }
+        [HttpPost("/sub")]
+        public ActionResult PostSub(int a, int b)
+        {
+            return Ok(a - b);
+        }
+        [HttpPost("/div")]
+        public ActionResult PostDiv(int a, int b)
+        {
+            return Ok(a / b);
+        }
+        [HttpPost("/mul")]
+        public ActionResult PostMul(int a, int b)
+        {
+            return Ok(a * b);
+        }
+
+    }
+}
